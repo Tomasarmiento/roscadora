8d2f479ad5e6aaf13c14db084478e5a3cf4293c1
Tomas Sarmiento <91971536+Tomasarmiento@users.noreply.github.com>
2022-01-04 18:03:03 -0300

    Se mandan valores a django POST (no los cambia ni actualiza los valores en la view). Levanta valores GET

--- backend/apps/parameters/urls.py
+++ backend/apps/parameters/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.ParameterListView.as_view(), name="parameters-list")
+    path('', views.ParameterListView.as_view(), name="parameters-list"),
+    path('update', views.update_parameters, name="parameters-update")
 ]
--- backend/apps/parameters/views.py
+++ backend/apps/parameters/views.py
@@ -1,6 +1,9 @@
+from django.db.models.expressions import Value
 from django.views.generic.list import ListView
-
+from django.shortcuts import render
 from apps.parameters.models import Parameter
+from django.http import HttpResponse
+
 
 
 class ParameterListView(ListView):
@@ -11,5 +14,26 @@ class ParameterListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        print("get successful")
         # context['now'] = timezone.now()
         return context
+
+    # Create your views here.
+def update_parameters(request):
+    if request.method == 'POST': print('hola')
+    response = HttpResponse()
+    response.status_code = 200
+    print(request.body)
+    
+    param = Parameter.objects.filter(part_model=1)(name='paso_de_rosca')
+   # param.value = (request.POST['paso_de_rosca'])
+    param.save()
+    
+    return render(request, "parametrosP1.html")
+
+
+
+
+
+
+    
