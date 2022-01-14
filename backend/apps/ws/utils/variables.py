13edbaeccf30a0f6b424f5d1e9d87df58eb1d1ed
Tomas Sarmiento <91971536+Tomasarmiento@users.noreply.github.com>
2022-01-14 16:09:36 -0300

    Agregadas funciones y variables para graficos. Agregado timeout de motor de avance encendido en clampeo

--- backend/apps/ws/utils/variables.py
+++ backend/apps/ws/utils/variables.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from apps.service.acdp.acdp import AcdpHeader
 from apps.service.acdp.messages_app import AcdpPc
 
@@ -10,21 +11,29 @@ class MicroState:
     cmd_ok          = True
 
     # Remote/Local digital I/O
-    rem_i_states    = []        # A dictionary with boolean values for each set
-    rem_o_states    = []
-    rem_i           = []        # Int number with flags values
-    rem_o           = []
-    loc_i_states    = None
-    loc_o_states    = None
-    loc_i           = None
-    loc_o           = None
+    rem_i_states        = []        # A dictionary with boolean values for each flag
+    rem_o_states        = []
+    rem_i               = []        # Int number with flags values
+    rem_o               = []
+    loc_i_states        = None
+    loc_o_states        = None
+    loc_i               = None
+    loc_o               = None
 
     # Flags
-    micro_flags     = {}            # Flags on the data part of the rx message
-    axis_flags      = [{}, {}, {}]  # Indexes corresponds with axis index
+    micro_flags         = {}            # Flags on the data part of the rx message
+    axis_flags          = [{}, {}, {}]  # Indexes corresponds with axis index
+
+    cabezal_on_timer    = datetime.now()
 
     # Measurements
-    axis_measures  = [{}, {}, {}]
+    axis_measures       = [{}, {}, {}]
+
+    # Graph
+    position_values     = []
+    torque_values       = []
+    graph_flag          = False
+    graph_duration      = 0
 
 
 class WsCodes:
