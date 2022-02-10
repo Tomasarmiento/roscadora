da06e139c9d12c6b3fa66d5f4fd504fbae0cbbd0
Tomas Sarmiento <91971536+Tomasarmiento@users.noreply.github.com>
2022-02-10 19:22:22 -0300

    Automatico y semi levantan el monitor. no esta ajustado el front. probar mensajes si anda

--- backend/apps/front/static/js/automatico.js
+++ backend/apps/front/static/js/automatico.js
@@ -276,8 +276,36 @@ window.onload = function() {
         InsertarTextoErrores(datosWs.mensajes_error);
     };
 
+    // Tabla de datos
+    const rpmActual = document.querySelector("#frRPM");
+    const torqueActual = document.querySelector("#fTorque");
+
+    // Datos Eje vertical
+    const posicionActualV = document.querySelector("#posVertical");
+    const velocidadActualV = document.querySelector("#velVertical");
+
+    // Datos Eje Horizontal
+    const posicionActualH = document.querySelector("#posHorizontal");
+    const velocidadActualH = document.querySelector("#velHorizontal");
+
+
+    //Cabezal
+    const cabezal = document.querySelector("#statusHead")
+    //Eje Lineal
+    const ejeLineal = document.querySelector("#statusLinealAxis")
+    //Descarga
+    const descarga = document.querySelector("#statusDownloader");
+    //Carga
+    const carga = document.querySelector("#statusLoader");
+    //Indexar
+    const indexar = document.querySelector("#statusIndex");
+    //Roscado
+    const roscado = document.querySelector("#statusRoscado")
+
+    
 
         if(datosWs){
+            console.log(datosWs)
             xAccelChartInstance.data.labels.push(new Date());            //(datosWs.cabezal_pos).toFixed(1);
             xAccelChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(datosWs.husillo_torque).toFixed(1)});
         if(updateCount > numberElements){
@@ -287,6 +315,89 @@ window.onload = function() {
         else updateCount++;
         xAccelChartInstance.update();
         }
+
+        //Monitor
+    rpmActual.innerHTML = datosWs.husillo_rpm.toFixed(1);
+    torqueActual.innerHTML = datosWs.husillo_torque.toFixed(1);
+
+    posicionActualV.innerHTML = datosWs.cabezal_pos.toFixed(1);
+    velocidadActualV.innerHTML = datosWs.cabezal_vel.toFixed(1);
+
+    posicionActualH.innerHTML = datosWs.avance_pos.toFixed(1);
+    velocidadActualH.innerHTML = datosWs.avance_vel.toFixed(1);
+
+    //cabezal
+    if (datosWs.estado_eje_carga == 'initial'){
+      (cabezal.className = "bg-success indicadorMon") && (cabezal.innerHTML = 'Cabezal <br/> Initial') 
+      }
+
+      else if (datosWs.estado_eje_carga == 'homing'){
+      (cabezal.className = "bg-warning indicadorMon") && (cabezal.innerHTML = 'Cabezal <br/> Homming')
+      }
+
+    else (cabezal.className = "bg-secondary indicadorMon");
+    
+    
+    //eje lineal
+    if (datosWs.estado_eje_avance == 'initial'){
+      (ejeLineal.className = "bg-success indicadorMon") && (ejeLineal.innerHTML = 'Eje lineal <br/> Initial') 
+      }
+
+      else if (datosWs.estado_eje_avance == 'homing'){
+      (ejeLineal.className = "bg-warning indicadorMon") && (ejeLineal.innerHTML = 'Eje lineal <br/> Homming')
+      }
+
+    else (ejeLineal.className = "bg-secondary indicadorMon");
+     
+     
+
+
+    //descarga
+    datosWs.remote_outputs[1].encender_bomba_hidraulica == true 
+     && datosWs.remote_inputs[1].clampeo_plato_expandido == true
+     && datosWs.remote_inputs[0].puntera_descarga_contraida == true
+     && datosWs.remote_inputs[0].brazo_descarga_expandido == true
+     && datosWs.remote_inputs[0].boquilla_descarga_expandida == true
+     && datosWs.remote_inputs[1].cupla_por_tobogan_descarga == true
+     && datosWs.remote_inputs[1].pieza_en_boquilla_descarga == true
+     && datosWs.remote_inputs[1].horiz_pinza_desc_contraido == true
+     && datosWs.remote_inputs[1].vert_pinza_desc_contraido == true
+     && datosWs.remote_inputs[0].pinza_descargadora_abierta == true && datosWs.remote_inputs[0].pinza_descargadora_cerrada == false
+    ? (descarga.className = "bg-success indicadorMon")
+    : (descarga.className = "bg-secondary indicadorMon");
+
+
+    //carga
+    datosWs.remote_outputs[1].encender_bomba_hidraulica == true
+     && datosWs.remote_inputs[1].clampeo_plato_expandido == true
+     && datosWs.remote_inputs[0].vertical_carga_contraido == true
+     && datosWs.remote_inputs[0].puntera_carga_expandida == false && datosWs.remote_inputs[0].puntera_carga_contraida == true
+     && datosWs.remote_inputs[0].brazo_cargador_expandido == true && datosWs.remote_inputs[0].brazo_cargador_contraido == false
+     && datosWs.remote_inputs[0].boquilla_carga_expandida == true
+     && datosWs.remote_inputs[1].presencia_cupla_en_cargador == true
+     && datosWs.remote_inputs[1].pieza_en_boquilla_carga == true
+    ? (carga.className = "bg-success indicadorMon")
+    : (carga.className = "bg-secondary indicadorMon");
+
+    //indexar
+    datosWs.remote_inputs[1].clampeo_plato_contraido == false && datosWs.remote_inputs[1].clampeo_plato_expandido == true
+     && datosWs.remote_inputs[1].acople_lubric_contraido == true
+     && datosWs.remote_inputs[0].puntera_descarga_expandida == false && datosWs.remote_inputs[0].puntera_descarga_contraida == true
+     && datosWs.remote_inputs[0].puntera_carga_expandida == false && datosWs.remote_inputs[0].puntera_carga_contraida == true
+     && datosWs.avance_pos.toFixed(1) >= datosWs.posicion_de_inicio
+     ? (indexar.className = "bg-success indicadorMon")
+     : (indexar.className = "bg-secondary indicadorMon");
+
+
+    //roscado
+    datosWs.remote_outputs[1].encender_bomba_hidraulica == true
+     && datosWs.remote_inputs[1].clampeo_plato_contraido == false && datosWs.remote_inputs[1].clampeo_plato_expandido == true
+     && datosWs.estado_eje_avance == 'initial'
+     && datosWs.cabezal_enable == false
+     && datosWs.avance_pos.toFixed(1) == datosWs.posicion_de_inicio
+     ? (roscado.className = "bg-success indicadorMon")
+     : (roscado.className = "bg-secondary indicadorMon");
+     
       
         
 }
